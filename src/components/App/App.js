8ea84fc9cfeb3c57ba155f8b58edd592db5d2681
Sam Mistele <smmstl0@gmail.com>
2022-04-26 10:53:58 -0600

    Add post

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,30 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import apiCall from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
 const App = () => {
-const [linkData, setLinkData] = useState([])
+  const [linkData, setLinkData] = useState([])
 
-useEffect(() => getData(), [])
+  useEffect(() => getAllData(), [])
 
-const getData = () => {
-  apiCall.getData('http://localhost:3001/api/v1/urls')
+  const getAllData = () => {
+    apiCall.getData('http://localhost:3001/api/v1/urls')
     .then(data => setLinkData(data.urls))
-}
+  }
+
+
+  const sendData = (urlData) => {
+    apiCall.postData(urlData)
+    .then((data) => getAllData())
+  }
 
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm sendData={sendData} />
         </header>
 
         <UrlContainer urls={linkData}/>
