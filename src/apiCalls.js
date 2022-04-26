dd8e6b88d84e3493e81df79576761c838fb20661
Sam Mistele <smmstl0@gmail.com>
2022-04-26 10:54:10 -0600

    Add post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,7 +8,14 @@ const apiCall = {
             return response.json()
           }
         })
-      }
-    }
+      },
+  postData(urlData) {
+    return fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(urlData)
+    })
+  }
+}
 
 export default apiCall
