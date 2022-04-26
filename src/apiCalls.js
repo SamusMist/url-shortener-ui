d0997fee22e2679cdd796a19d9c2c16a6e30c802
Sam Mistele <smmstl0@gmail.com>
2022-04-26 09:28:54 -0600

    Refactor api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,14 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
-}
+const apiCall = {
+  getData(url) {
+    return fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("error")
+          } else {
+            return response.json()
+          }
+        })
+      }
+    }
+
+export default apiCall
